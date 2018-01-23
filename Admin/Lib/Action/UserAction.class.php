80b8b3826d7d1d63ad9af6e82a99f140b396127d
wAngxuedan <729731052@qq.com>
2018-01-23 23:44:56 +0800

    后台用户管理查询和分页功能

--- Admin/Lib/Action/UserAction.class.php
+++ Admin/Lib/Action/UserAction.class.php
@@ -1,10 +1,15 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
+import('ORG.Util.Page');
 class UserAction extends Action {
     public function index(){
         $m=M('User');
-        $array=$m->select();
+        $count=$m->count();
+        $Page=new Page($count,10);
+        $show=$Page->show();
+        $array=$m->limit($Page->firstRow.','.$Page->listRows)->select();
         $this->assign('user',$array);
+        $this->assign('show',$show);
         $this->display();
     } 
     public function changeUserInfo(){ 
@@ -24,5 +29,19 @@ class UserAction extends Action {
         else{
          	  $this->error('修改用户信息失败');
          	}
+    }
+    public function search(){
+      $string=$_POST['condition'];
+      $m=M('User');
+      $condition['account']=array('like',"%$string%");
+      $condition['username']=array('like',"%$string%");
+      $condition['_logic'] = 'OR';
+      $count=$m->where($condition)->count();      
+      $Page=new Page($count,10);
+      $show=$Page->show();
+      $result=$m->limit($Page->firstRow.','.$Page->listRows)->where($condition)->select();
+      $this->assign('user',$result);
+      $this->assign('show',$show);
+      $this->display();
     }
 }
